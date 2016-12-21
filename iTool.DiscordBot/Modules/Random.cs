a6e197f6407d0d3603c2dd40eebf6655b8cdc1e2
Bond-009 <lode2willems@hotmail.com>
2016-12-21 13:04:10 +0100

    Added !cat Command

---
+++ iTool.DiscordBot/Modules/Random.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using System.Xml.Linq;
+using Discord;
+using Discord.Commands;
+using Newtonsoft.Json;
+
+namespace iTool.DiscordBot.Modules
+{
+	public class Random : ModuleBase
+	{
+		[Command("cat")]
+		[Summary("Gets a random cat image")]
+		public async Task Cat()
+		{
+			XDocument xDoc = JsonConvert.DeserializeXNode(await (new HttpClient().GetStringAsync("http://random.cat/meow")), "root");
+			await ReplyAsync("", false, new EmbedBuilder()
+			{
+				Color = new Color(3, 144, 255),
+				ImageUrl = xDoc.Element("root").Element("file").Value
+			});
+		}
+	}
+}
