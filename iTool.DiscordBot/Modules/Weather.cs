6f89b875d89bd64ef1fc21e4386b5b8731908747
Bond-009 <lode2willems@hotmail.com>
2016-12-30 19:34:35 +0100

    Started adding !weather command

--- iTool.DiscordBot/Modules/Weather.cs
+++ iTool.DiscordBot/Modules/Weather.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using iTool.DiscordBot.Prefrences;
+using System;
+using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using OpenWeatherNet;
 
@@ -8,10 +11,27 @@ namespace iTool.DiscordBot.Modules
 	{
 		[Command("weather")]
 		[Summary("Gets the weather info")]
-		public async Task GetWeather([Remainder] string input)
+		public async Task GetWeather(string input)
 		{
 			//TODO: Add weather
-			//OpenWeatherClient client = new OpenWeatherClient();
+			OpenWeatherClient client = new OpenWeatherClient(Settings.ApiKeys.OpenWeatherMapKey);
+			WeatherInfo weather = await client.GetByCityNameAsync(input);
+			weather.temperature.ToCelsius(); //TODO: Add temperaturescale setting
+
+			EmbedBuilder b = new EmbedBuilder()
+			{
+				Title = weather.city.name + " " + weather.city.country,
+				Color = new Color(3, 144, 255),
+				ThumbnailUrl = weather.iconLocation
+			};
+			b.AddField(delegate (EmbedFieldBuilder f)
+			{
+				f.Name = "Temperature";
+				f.Value = $"Max: {weather.temperature.max} {weather.temperature.unit}" + Environment.NewLine +
+							$"Gem: {weather.temperature.value} {weather.temperature.unit}" + Environment.NewLine +
+							$"Min: {weather.temperature.min} {weather.temperature.unit}";
+			});
+			await ReplyAsync("", false, b);
 		}
 	}
 }
